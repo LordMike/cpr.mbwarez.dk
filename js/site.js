65b15107994698ce8d906c89215442cb72b5584a
Michael Bisbjerg <michael@mbwarez.dk>
2017-06-14 23:20:46 +0200

    Optimize calculations

--- js/site.js
+++ js/site.js
@@ -18,27 +18,28 @@ cprApp.controller('CprController', ['$scope', function CprController($scope) {
 
     this.candidateList = [];
 
-    this.getGender = function (number) {
-        // Number in form '0123456789' (no "-") (string)
-        if (parseInt(number[9]) % 2 == 0)
+    this.getGender = function (cprPart) {
+        // cprPart in number form
+        if (cprPart % 2 == 0)
             return 'female';
 
         return 'male';
     }
-
-    this.checkCpr = function (number, gender) {
-        // Number in form '0123456789' (no "-")
+    
+    this.checkCprPart = function (initialSum, partialNumber) {
+        // InitialSum is the sum of the first 6 digits
+        // PartialNumber is the last 4 digits, in string form
         var sum = 0;
 
-        for (var i = 0; i < 10; i++) {
-            sum += parseInt(number[i]) * controlDigits[i];
+        for (var i = 0; i < 4; i++) {
+            sum += parseInt(partialNumber[i]) * controlDigits[6 + i];
         }
 
         if (sum % 11 != 0) {
             return false;
         }
 
-        return gender == 'either' || gender == this.getGender(number);
+        return true;
     }
 
     this.updateCandidates = function () {
@@ -56,19 +57,38 @@ cprApp.controller('CprController', ['$scope', function CprController($scope) {
             upper = 9999;
         }
 
+        // Determine desired gender and increment
+        var increment = 2;
+        if (gender == 'either') {
+            // In case of both genders, all numbers are valid
+            increment = 1;
+        }
+        else if (gender == 'male') {
+            // Males are uneven, so we shift the start by 1
+            lower += 1;
+        }
+
         var res = [];
 
         // Format prefix: DDMMYY
         var datePrefix = date.getDate().pad(2) + (date.getMonth() + 1).pad(2) + new String(date.getFullYear()).substr(2);
 
-        for (var i = lower; i < upper; i++) {
-            var number = datePrefix + i.pad(4);
+        // Determine sum of date prefix
+        var datePrefixSum = 0;
+        for (var i = 0; i < 6; i++) {
+            datePrefixSum += parseInt(datePrefix[i]) * controlDigits[i];
+        }
 
-            if (!this.checkCpr(number, gender))
+        for (var i = lower; i < upper; i += increment) {
+            var partNumer = i.pad(4);
+
+            if (!this.checkCprPart(datePrefixSum, partNumer))
                 continue;
 
+            var number = datePrefix + partNumer;
+
             var numberGender = this.getGender(number);
-            var actualNumber = datePrefix + '-' + i.pad(4);
+            var actualNumber = datePrefix + '-' + partNumer;
 
             // Valid CPR
             res.push({
